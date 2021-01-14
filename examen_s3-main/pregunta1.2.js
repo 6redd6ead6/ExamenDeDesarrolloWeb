b3e1782aca6fb209cf5993794a825609ddfcce5a
6redd6ead6 <77453758+6redd6ead6@users.noreply.github.com>
2021-01-14 12:15:56 -0500

    Delete examen_s3-main directory

--- examen_s3-main/pregunta1.2.js
+++
@@ -1,7 +0,0 @@
-
-
-
-function bien (){
-    var nombre="jossep aguirre sulca";
-    console.log("bienvenido "+nombre);
-     }
--- examen_s3-main/pregunta1.3.js
+++
@@ -1,8 +0,0 @@
-function inpus (){
-    total=100;
-    subtotal=total/1.18;
-    igv=subtotal*0.18;
-    console.log("el total es "+total);
-    console.log("el subtotal es "+subtotal);
-    console.log("el igv es "+igv);
-     }
